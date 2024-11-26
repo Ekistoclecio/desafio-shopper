04c7558b62f86b7b996c0fd854deea7b74f2ee45
ekistoclecio <ekistoclecio1999@gmail.com>
2024-11-26 19:42:23 -0300

    feat(frontend>theme): update color palette for primary and grey shades

--- frontend/src/theme/index.ts
+++ frontend/src/theme/index.ts
@@ -3,24 +3,22 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#167c7e',
-      light: '#5ba9aa',
-      dark: '#0d5052',
+      light: '#b2cce8',
+      main: '#0056B4',
+      dark: '#003c7e',
     },
-    background: {
-      default: '#e8f1f1',
-    },
+
     grey: {
-      50: '#FAFAFA',
-      100: '#F5F5F5',
-      200: '#EEEEEE',
-      300: '#E0E0E0',
-      400: '#BDBDBD',
-      500: '#9E9E9E',
-      600: '#757575',
-      700: '#616161',
-      800: '#424242',
-      900: '#212121',
+      50: '#f2f2f2',
+      100: '#e6e6e6',
+      200: '#cccccc',
+      300: '#b2b2b2',
+      400: '#999999',
+      500: '#808080',
+      600: '#666666',
+      700: '#4c4c4c',
+      800: '#333333',
+      900: '#1a1a1a',
     },
   },
 });
