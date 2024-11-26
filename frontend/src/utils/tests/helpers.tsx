4025328a57d704fb8a01ea8b23ee1879c8cb498f
ekistoclecio <ekistoclecio1999@gmail.com>
2024-11-26 19:43:15 -0300

    tests(frontend): wrap renderWithTheme in MemoryRouter for routing context

--- frontend/src/utils/tests/helpers.tsx
+++ frontend/src/utils/tests/helpers.tsx
@@ -2,12 +2,15 @@ import { CssBaseline, GlobalStyles, ThemeProvider } from '@mui/material';
 import { theme } from '@/theme';
 import { render, RenderResult } from '@testing-library/react';
 import { globalStyle } from '@/styles/global';
+import { MemoryRouter } from 'react-router';
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <GlobalStyles styles={globalStyle} />
-      {children}
-    </ThemeProvider>,
+    <MemoryRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <GlobalStyles styles={globalStyle} />
+        {children}
+      </ThemeProvider>
+    </MemoryRouter>,
   );
