6985b451293ed5acaf642de539a02c590d4a6849
ekistoclecio <ekistoclecio1999@gmail.com>
2024-11-27 13:20:25 -0300

    feat(styles): customize global scrollbar styles

--- frontend/src/styles/global.ts
+++ frontend/src/styles/global.ts
@@ -5,6 +5,28 @@ export const globalStyle = (theme: Theme) => ({
     boxSizing: 'border-box',
     margin: 0,
     padding: 0,
+
+    '&::-webkit-scrollbar': {
+      width: '5px',
+      backgroundColor: theme.palette.grey[100],
+    },
+    '&::-webkit-scrollbar-thumb': {
+      backgroundColor: theme.palette.grey[500],
+      borderRadius: '24px',
+    },
+    '&::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: theme.palette.grey[700],
+    },
+    '&::-webkit-scrollbar-track': {
+      backgroundColor: theme.palette.grey[200],
+      borderRadius: '24px',
+    },
+    '&::-webkit-scrollbar-corner': {
+      backgroundColor: theme.palette.grey[300],
+    },
+    '&::-webkit-scrollbar-button': {
+      display: 'none',
+    },
   },
   html: {
     fontSize: '62.5%',
